f7fa7438946e37cd8c020cc32ef39e020737de43
GracePearcey <pearcey.grace@gmail.com>
2023-06-05 14:39:29 -0230

    project now compiles, had to add extern 'C' to app_main

---
+++ main/test.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+
+extern "C" void app_main(void)
+{
+    printf("Hello world!\n");
+
+}
