30a1e6855a15daabbcc8972f3ba582844ffc93a2
GracePearcey <pearcey.grace@gmail.com>
2023-06-12 11:57:28 -0230

    can read and write with RTOS tasks, but can't juggle them

--- main/NMEA_msg.h
+++ main/NMEA_msg.h
@@ -1,16 +1,13 @@
 #ifndef NMEA_MSG_H
 #define NMEA_MSG_H
-
-#include <string>
+#include <vector>
 
 struct NMEA_msg {
     uint32_t PGN : 18;
     uint8_t src;
-    std::string name;
     uint8_t priority : 3;
     int length;
-    int numFields;
-    uint8_t data[223];
+    std::vector<uint8_t> data;
 };
 
-#endif //NMEA_MSG_H
+#endif //NMEA_MSG_Hcode 
