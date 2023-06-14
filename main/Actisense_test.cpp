fecaf6d5b47997d47590d6cb7c36e3f5a4b6b973
GracePearcey <pearcey.grace@gmail.com>
2023-06-14 15:05:04 -0230

    trying to user Actisense class to read and write

--- main/Actisense_test.cpp
+++ main/Actisense_test.cpp
@@ -9,14 +9,15 @@
 #include <NMEA2000.h>
 #include "ESP32N2kStream.h"
 #include <N2kMessages.h>
+#include <ActisenseReader.h>
 //#define ESP32_CAN_TX_PIN (gpio_num_t) 32 // from sdkconfig (idf menuconfig)
 //#define ESP32_CAN_RX_PIN (gpio_num_t) 34
 
 using tN2kSendFunction=void (*)();
 ESP32N2kStream serial;
 
-static const char* TAG = "message_sending.cpp";
-tNMEA2000_esp32 NMEA2000_rx;
+static const char* TAG = "Actisense_test.cpp";
+tActisenseReader ActisenseReader;
 tNMEA2000_esp32 NMEA2000_tx;
 static TaskHandle_t N2K_tx_task_handle = NULL;
 
@@ -112,7 +113,6 @@ void SendN2kMessages() {
 // This is a FreeRTOS task
 void N2K_tx_task(void *pvParameters)
 {
-    TickType_t lastWakeTime = xTaskGetTickCount();
     ESP_LOGI(TAG, "Starting task");
     
     NMEA2000_tx.SetN2kCANMsgBufSize(8);
@@ -162,37 +162,20 @@ static TaskHandle_t N2K_rx_task_handle = NULL;
 // This is a FreeRTOS task
 void N2K_rx_task(void *pvParameters)
 {
-    TickType_t lastWakeTime = xTaskGetTickCount();
-    ESP_LOGI(TAG, "Starting task");
     
-    NMEA2000_rx.SetN2kCANMsgBufSize(8);
-    NMEA2000_rx.SetN2kCANReceiveFrameBufSize(223);// not sure what this should be set to
+    ESP_LOGI(TAG, "Starting task");
+    ActisenseReader.SetReadStream(&serial);
+    ActisenseReader.SetDefaultSource(75);
 
-    NMEA2000_rx.SetForwardStream(new ESP32N2kStream()); // will need to change to WASM
-    NMEA2000_rx.SetForwardType(tNMEA2000::fwdt_Text);   // Show in clear text
-    // NMEA2000.EnableForward(false);                 // Disable all msg forwarding to USB (=Serial)
-
-    //NMEA2000.SetMsgHandler(HandleNMEA2000Msg);
-
-    //do I need to do anything for group functions?
-
-    // If you also want to see all traffic on the bus use N2km_ListenAndNode instead of N2km_NodeOnly below
-    NMEA2000_rx.SetMode(tNMEA2000::N2km_ListenOnly, 22);//does 22 do anything in ListenOnly mode?
-
-    // Here we tell, which PGNs we transmit and receive we want to receive all!
-    //NMEA2000.ExtendTransmitMessages(TransmitMessages, 0);
-    //NMEA2000.ExtendReceiveMessages(ReceiveMessages, 0);
-
-    // Define OnOpen call back. This will be called, when CAN is open and system starts address claiming.
-    NMEA2000_rx.Open();
     for (;;)
     {
         // put your main code here, to run repeatedly:
         //TaskN2kBinStatus();
         
-        NMEA2000_rx.ParseMessages();
+        ActisenseReader.ParseMessages();
         //vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(10));
         //vTaskDelay(pdMS_TO_TICKS(1000));
+
 
     }
     vTaskDelete(NULL); // should never get here...
@@ -208,7 +191,7 @@ extern "C" int app_main(void)
         "Sending_task",           // A descriptive name for the task for debugging.
         3072,                 // size of the task stack in bytes.
         NULL,                 // Optional pointer to pvParameters
-        tskIDLE_PRIORITY + 5, // priority at which the task should run
+        tskIDLE_PRIORITY, // priority at which the task should run
         &N2K_tx_task_handle      // Optional pass back task handle
     );
     if (N2K_tx_task_handle == NULL)
@@ -217,21 +200,21 @@ extern "C" int app_main(void)
         result = ESP_ERR_NO_MEM;
         goto err_out;
     }
-    //ESP_LOGV(TAG, "create rx task");
-    //xTaskCreate(
-    //    &N2K_rx_task,            // Pointer to the task entry function.
-    //    "Reading_task",           // A descriptive name for the task for debugging.
-    //    3072,                 // size of the task stack in bytes.
-    //    NULL,                 // Optional pointer to pvParameters
-    //    tskIDLE_PRIORITY + 6, // priority at which the task should run
-    //    &N2K_rx_task_handle      // Optional pass back task handle
-    //);
-    //if (N2K_rx_task_handle == NULL)
-    //{
-    //    ESP_LOGE(TAG, "Unable to create task.");
-    //    result = ESP_ERR_NO_MEM;
-    //    goto err_out;
-    //}
+    ESP_LOGV(TAG, "create rx task");
+    xTaskCreate(
+        &N2K_rx_task,            // Pointer to the task entry function.
+        "Reading_task",           // A descriptive name for the task for debugging.
+        3072,                 // size of the task stack in bytes.
+        NULL,                 // Optional pointer to pvParameters
+        tskIDLE_PRIORITY, // priority at which the task should run
+        &N2K_rx_task_handle      // Optional pass back task handle
+    );
+    if (N2K_rx_task_handle == NULL)
+    {
+        ESP_LOGE(TAG, "Unable to create task.");
+        result = ESP_ERR_NO_MEM;
+        goto err_out;
+    }
 
 
 
